1080429e36266d582c5504f55d7ac8eb38915a81
Cristian Pérez García <cristian.perez@cmv.mx>
2020-05-28 01:24:49 -0500

    se agregan archivos nuevos

---
+++ NotificacionPush/app/src/main/java/com/example/notificacionpush/Resultado.kt
@@ -0,0 +1,8 @@
+package com.example.jsonandhttprequest
+
+import com.example.notificacionpush.Resultados
+
+//class Usuario (val id:Int, val name: String, val username:String, val email: String)
+//class Usuario (val id:Int, val name: String, val username:String, val email: String, val address: Object)
+//class Usuario (val id:Int, val name: String, val username:String, val email: String, val address: Address)
+class Resultado(val results: Resultados)
